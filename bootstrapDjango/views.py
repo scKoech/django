62f190b8d87355925b1f28c5cc79689144b77776
scKoech <cheropkoech04@gmail.com>
2023-02-24 08:32:59 +0300

    initial commit

---
+++ bootstrapDjango/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def menu(request):
+    return render(request, 'menu.html')
+def service(request):
+    return render(request, 'service.html')
+def team(request):
+    return render(request, 'team.html')
+def testimonials(request):
+    return render(request, 'testimonial.html')
